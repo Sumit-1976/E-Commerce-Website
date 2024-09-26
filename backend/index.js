39a7d4263203c9ada588e332a8441f2267bd2bda
Sumit Kar <sumitcoder2004@gmail.com>
2024-09-26 23:12:36 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+    origin: ['http://localhost:3000', 'https://urbancart-sigma.vercel.app'],
+    credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
