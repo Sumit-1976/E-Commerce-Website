b656fd78294b3b45b52c13d41f4082a38ef695ca
Sumit Kar <sumitcoder2004@gmail.com>
2024-09-26 23:25:15 +0530

    secure

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcryptjs');
 const userModel = require('../../models/userModel');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 async function userSignInController(req, res) {
     try {
         const { email, password } = req.body;
 
+        // Input validation
         if (!email) {
             return res.status(400).json({ error: 'Please provide your email!' });
         }
@@ -14,33 +15,40 @@ async function userSignInController(req, res) {
             return res.status(400).json({ error: 'Please provide a password!' });
         }
 
-        const user = await userModel.findOne({email});
+        // Find the user by email
+        const user = await userModel.findOne({ email });
 
         if (!user) {
             return res.status(404).json({ error: 'User not found!' });
         }
 
-        const checkPassword = await bcrypt.compareSync(password, user.password);
-        
-        if (checkPassword) {
-            const tokenData = {
-                _id : user._id,
-                email : user.email,
-            }
-            const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, {expiresIn: 60*60*8});
+        // Compare password
+        const checkPassword = await bcrypt.compare(password, user.password);
 
+        if (checkPassword) {
+            // Token payload and creation
+            const tokenData = {
+                _id: user._id,
+                email: user.email,
+            };
+            const token = jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: '8h' });
+
+            // Cookie options with conditional security for production
             const tokenOption = {
-                httpOnly : true,
-                secure : true
-            }
-            res.cookie("token",token,tokenOption).json({
-                message : "Login Succesfully!",
-                data : token,
-                success : true,
-                error : false
-            })
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', // Only secure in production
+                sameSite: 'None', // Allows cross-origin cookies
+            };
+
+            // Send cookie and response
+            res.cookie('token', token, tokenOption).json({
+                message: 'Login successfully!',
+                data: token,
+                success: true,
+                error: false,
+            });
         } else {
-            throw new Error("Please check the entered password!")
+            throw new Error('Incorrect password, please try again!');
         }
 
     } catch (error) {
@@ -52,4 +60,4 @@ async function userSignInController(req, res) {
     }
 }
 
-module.exports = userSignInController
+module.exports = userSignInController;
